0ecb3034d1797601da1d10df4f6c89c903175e44
KookaS <olivier.charrez@hotmail.com>
2020-11-12 16:59:13 +0100

    volumes

--- src/database/upload.ts
+++ src/database/upload.ts
@@ -11,6 +11,7 @@ export const uploadFile = () => {
     const storage = multer.diskStorage({
         destination: (req, file, callback) => {
             const path = `${process.env.API_IMG}`
+            console.log(path)
             fs.mkdirSync(path, { recursive: true })
             callback(null, path)
         },
